c142b3282bff0e4be3fb6bc0c6afae336affd63e
Ido <idonew2@gmail.com>
2026-04-29 23:07:51 +0300

    Added more questions

---
+++ Arrays & Hashing/121.BestTimetoBuyandSellStock.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        low = 0
+        high = 1
+        max_profit = 0
+        
+        while high<len(prices):
+            if prices[high] <= prices[low]:
+                low= high
+            else:
+                max_profit = max(max_profit,prices[high]- prices[low])
+            high+=1
+            
+        return max_profit
