0b81486235460474973fb8965ebee963219225da
Ido <idonew2@gmail.com>
2026-04-30 11:19:12 +0300

    Added comments explaining my solution

--- Arrays & Hashing/88.MergeSortedArray.py
+++ Arrays & Hashing/88.MergeSortedArray.py
@@ -3,17 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        end1 = m-1
-        end2 = n-1
-        end_all = m+n-1
+        # Pointers starting from the end of each array
+        end1 = m - 1        # Last valid element in nums1
+        end2 = n - 1        # Last element in nums2
+        end_all = m + n - 1 # Last position in the merged nums1
         
-        while end2>=0:
-            if end1>=0 and nums1[end1] > nums2[end2]:
+        # Merge from the end (right to left) to avoid overwriting nums1 elements
+        # This works because nums1 has extra space at the end
+        while end2 >= 0:
+            if end1 >= 0 and nums1[end1] > nums2[end2]:
+                # nums1's element is larger - place it at the end
                 nums1[end_all] = nums1[end1]
-                end1 -=1
+                end1 -= 1
             else:
+                # nums2's element is larger or nums1 is exhausted
                 nums1[end_all] = nums2[end2]
-                end2-=1
-            end_all -=1
+                end2 -= 1
+            end_all -= 1
+
+# T(n) = O(m + n) - single pass through all elements
+# S(n) = O(1) - only using pointer variables, no extra space
             
         
